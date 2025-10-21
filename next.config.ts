1c576d16c47f8a44ba745eb930c2c67a7f456017
fmillanjs <fmillan.js@gmail.com>
2025-10-21 16:06:03 -0600

    restarting project

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: 'standalone',
-  serverExternalPackages: ['@prisma/client'],
-  transpilePackages: ['@trpc/server', '@trpc/client', '@trpc/next', '@trpc/react-query'],
-};
-
-export default nextConfig;
